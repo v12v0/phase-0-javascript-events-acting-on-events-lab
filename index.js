dc6084ed372cd3d81d7ba0a793b79f97687c8a5f
Vi Vo <aaronvo118@gmail.com>
2021-12-16 12:55:36 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+// dodger left 
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 4}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+//dodger right 
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+  if (left < 360) {
+    dodger.style.left = `${left + 4}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+
+});
+
+
